ba78a414fb65e5d8adf5de566f025c66480a5d1a
Roma <138378823+Cubball@users.noreply.github.com>
2023-10-25 14:06:23 +0300

    Add migration

---
+++ src/EcoStore.DAL/Migrations/20231025105450_AddPayment.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcoStore.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPayment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentId",
+                table: "Orders",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Amount = table.Column<int>(type: "int", nullable: false),
+                    Currency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_PaymentId",
+                table: "Orders",
+                column: "PaymentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders",
+                column: "PaymentId",
+                principalTable: "Payments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Orders_Payments_PaymentId",
+                table: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Orders_PaymentId",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentId",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "Orders",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
