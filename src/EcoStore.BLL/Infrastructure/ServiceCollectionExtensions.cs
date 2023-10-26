57b8a794a81f93232de23708c2c7aaa67ef25317
Roma <138378823+Cubball@users.noreply.github.com>
2023-10-26 22:31:42 +0300

    Register new validators

--- src/EcoStore.BLL/Infrastructure/ServiceCollectionExtensions.cs
+++ src/EcoStore.BLL/Infrastructure/ServiceCollectionExtensions.cs
@@ -13,6 +13,8 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddScoped<IClock, Clock>();
+        services.AddScoped<IValidator<CancelOrderByAdminDTO>, CancelOrderByAdminValidator>();
+        services.AddScoped<IValidator<CancelOrderByUserDTO>, CancelOrderByUserValidator>();
         services.AddScoped<IValidator<CreateBrandDTO>, CreateBrandValidator>();
         services.AddScoped<IValidator<CreateCategoryDTO>, CreateCategoryValidator>();
         services.AddScoped<IValidator<CreateOrderDTO>, CreateOrderValidator>();
