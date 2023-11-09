3d7ea12e40c13c5afb8edf1ad85a93580fab82f7
Roma <138378823+Cubball@users.noreply.github.com>
2023-11-09 09:58:30 +0200

    Begin working on the cart functionality

--- src/EcoStore.Presentation/wwwroot/js/site.js
+++ src/EcoStore.Presentation/wwwroot/js/site.js
@@ -2,3 +2,48 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const getCartCookieKey = (userId) => `cart_for_${userId}=`;
+
+const getCartCookie = (userId) => {
+    const cookiesString = document.cookie;
+    const key = getCartCookieKey(userId);
+    const cookies = cookiesString.split(';');
+    for (const cookie of cookies) {
+        const trimmedCookie = cookie.trim();
+        if (trimmedCookie.startsWith(key)) {
+            const cartCookie = trimmedCookie.substring(key.length);
+            return JSON.parse(cartCookie);
+        }
+    }
+
+    return {};
+}
+
+const setCartCookieForProduct = (userId, productId, productCount) => {
+    const cartCookie = getCartCookie(userId);
+    cartCookie[productId] = productCount;
+    const date = new Date()
+    date.setTime(date.getTime() + getMilisecondsInDays(7))
+    setCookie(getCartCookieKey(userId), JSON.stringify(cartCookie), date.toUTCString());
+}
+
+const removeProductFromCartCookie = (userId, productId) => {
+    const cartCookie = getCartCookie(userId)
+    const cartCookieKey = getCartCookieKey(userId);
+    delete cartCookie[productId]
+    const date = new Date()
+    if (Object.keys(cartCookie).length > 0) {
+        date.setTime(date.getTime() + getMilisecondsInDays(7))
+    } else {
+        date.setTime(0)
+    }
+
+    setCookie(cartCookieKey, JSON.stringify(cartCookie), date.toUTCString());
+}
+
+const setCookie = (keyWithEquals, value, expiresIn) => {
+    document.cookie = `${keyWithEquals}${value};expires=${expiresIn};path=/`;
+}
+
+const getMilisecondsInDays = (days) => 24 * 60 * 60 * 1000 * days;
